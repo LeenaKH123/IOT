fb52e6276d1068a17b8fe1ad130698c8ad9a4576
Leena <engleena@yahoo.com>
2021-12-31 23:40:06 +0300

    required CSV file generated

--- acrel.py
+++ acrel.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import csv
+
 # Get token
 # NewRequest = {'Content-Type': 'application/x-www-form-urlencoded'}
 # url = 'http://iot.acrel-eem.com/oversea/entry/home/getRelayToken.action'
@@ -43,5 +45,27 @@ body = {
 # Adding empty header as parameters are being sent in payload
 headers = {'Content-Type': 'application/x-www-form-urlencoded', 'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTYwMDkwNzF9.HuBFQFsS3FVN-rMPsgXHzk2E4bOqvZp6uZhs3eDd9Ng', 'mark':'relay'}
 r = requests.post(url, data=body, headers=headers)
-print(r.status_code)
-print(r.json())
+# print(r.status_code)
+output = (r.json())
+output1 = output['data']
+print(output1)
+exc_head = []
+exc_data = []
+for key in output1:
+    exc_head.append(key)
+    exc_data.append(output1[key])
+
+with open('data.csv', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(exc_head)
+
+    # write the data
+    writer.writerow(exc_data)
+# print(exc_head)
+# print(exc_data)
+    
+
+
+
