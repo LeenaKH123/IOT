fd6aefaa6fbfc4947f444c28fa34249e70b09d68
Leena <engleena@yahoo.com>
2021-12-31 20:33:30 +0300

    got the data

--- acrel.py
+++ acrel.py
@@ -10,6 +10,7 @@ import json
 # output = (response.json())
 # output1 = output["data"]
 # print(output1)
+
 # Get power usage
 # NewRequest2 = {'Content-Type': 'application/x-www-form-urlencoded', 'token':'f25d1b92c5014f76a99c7adede88c4f3', 'mark': 'relay'}
 # url2 = 'http://iot.acrel-eem.com/oversea/energy/getEnergyTable.action'
@@ -20,3 +21,27 @@ import json
 # output2 = (response.json())
 # output12 = output2["data"]
 # print(output12)
+# -----------
+# get meter real time data
+# NewRequest = {'Content-Type': 'application/x-www-form-urlencoded', 'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTYwMDkwNzF9.HuBFQFsS3FVN-rMPsgXHzk2E4bOqvZp6uZhs3eDd9Ng', 'mark':'relay'}
+# url = 'http://iot.acrel-eem.com/oversea/dataMonitor/getMonitor.action'
+# x = requests.post(url, json= NewRequest)
+# print(x.status_code, x.reason)
+# print(x.content)
+# response = requests.get("http://iot.acrel-eem.com/oversea/dataMonitor/getMonitor.action")
+# output = (response.json())
+# output1 = output["data"]
+# print(output1)
+# ------------------------------------
+
+
+url = 'http://iot.acrel-eem.com/oversea/dataMonitor/getMonitor.action'
+body = {
+    'SwicthId': '12109137160038',
+    'MeterSN': '12109137160011'
+}
+# Adding empty header as parameters are being sent in payload
+headers = {'Content-Type': 'application/x-www-form-urlencoded', 'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTYwMDkwNzF9.HuBFQFsS3FVN-rMPsgXHzk2E4bOqvZp6uZhs3eDd9Ng', 'mark':'relay'}
+r = requests.post(url, data=body, headers=headers)
+print(r.status_code)
+print(r.json())
