6c736cf0ac25e50804dac428c1514c9ef08032af
Leena <engleena@yahoo.com>
2021-12-31 23:43:15 +0300

    Final code and CSV file

--- acrel.py
+++ acrel.py
@@ -1,54 +1,17 @@
 import requests
 import json
 import csv
-
-# Get token
-# NewRequest = {'Content-Type': 'application/x-www-form-urlencoded'}
-# url = 'http://iot.acrel-eem.com/oversea/entry/home/getRelayToken.action'
-# x = requests.post(url, json= NewRequest)
-# print(x.status_code, x.reason)
-# print(x.content)
-# response = requests.get("http://iot.acrel-eem.com/oversea/entry/home/getRelayToken.action")
-# output = (response.json())
-# output1 = output["data"]
-# print(output1)
-
-# Get power usage
-# NewRequest2 = {'Content-Type': 'application/x-www-form-urlencoded', 'token':'f25d1b92c5014f76a99c7adede88c4f3', 'mark': 'relay'}
-# url2 = 'http://iot.acrel-eem.com/oversea/energy/getEnergyTable.action'
-# x2 = requests.post(url2, json= NewRequest2)
-# print(x2.status_code, x2.reason)
-# print(x2.content)
-# response = requests.get("http://iot.acrel-eem.com/oversea/energy/getEnergyTable.action")
-# output2 = (response.json())
-# output12 = output2["data"]
-# print(output12)
-# -----------
-# get meter real time data
-# NewRequest = {'Content-Type': 'application/x-www-form-urlencoded', 'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTYwMDkwNzF9.HuBFQFsS3FVN-rMPsgXHzk2E4bOqvZp6uZhs3eDd9Ng', 'mark':'relay'}
-# url = 'http://iot.acrel-eem.com/oversea/dataMonitor/getMonitor.action'
-# x = requests.post(url, json= NewRequest)
-# print(x.status_code, x.reason)
-# print(x.content)
-# response = requests.get("http://iot.acrel-eem.com/oversea/dataMonitor/getMonitor.action")
-# output = (response.json())
-# output1 = output["data"]
-# print(output1)
-# ------------------------------------
-
-
 url = 'http://iot.acrel-eem.com/oversea/dataMonitor/getMonitor.action'
 body = {
     'SwicthId': '12109137160038',
     'MeterSN': '12109137160011'
 }
-# Adding empty header as parameters are being sent in payload
 headers = {'Content-Type': 'application/x-www-form-urlencoded', 'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTYwMDkwNzF9.HuBFQFsS3FVN-rMPsgXHzk2E4bOqvZp6uZhs3eDd9Ng', 'mark':'relay'}
 r = requests.post(url, data=body, headers=headers)
 # print(r.status_code)
 output = (r.json())
 output1 = output['data']
-print(output1)
+# print(output1)
 exc_head = []
 exc_data = []
 for key in output1:
