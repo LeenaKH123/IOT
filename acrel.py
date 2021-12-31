bf54310c9af4b109afca3f31b324ac1d3a08b6df
Leena <engleena@yahoo.com>
2021-12-31 19:12:07 +0300

    reading data in progress

---
+++ acrel.py
@@ -0,0 +1,22 @@
+import requests
+import json
+# Get token
+# NewRequest = {'Content-Type': 'application/x-www-form-urlencoded'}
+# url = 'http://iot.acrel-eem.com/oversea/entry/home/getRelayToken.action'
+# x = requests.post(url, json= NewRequest)
+# print(x.status_code, x.reason)
+# print(x.content)
+# response = requests.get("http://iot.acrel-eem.com/oversea/entry/home/getRelayToken.action")
+# output = (response.json())
+# output1 = output["data"]
+# print(output1)
+# Get power usage
+# NewRequest2 = {'Content-Type': 'application/x-www-form-urlencoded', 'token':'f25d1b92c5014f76a99c7adede88c4f3', 'mark': 'relay'}
+# url2 = 'http://iot.acrel-eem.com/oversea/energy/getEnergyTable.action'
+# x2 = requests.post(url2, json= NewRequest2)
+# print(x2.status_code, x2.reason)
+# print(x2.content)
+# response = requests.get("http://iot.acrel-eem.com/oversea/energy/getEnergyTable.action")
+# output2 = (response.json())
+# output12 = output2["data"]
+# print(output12)
